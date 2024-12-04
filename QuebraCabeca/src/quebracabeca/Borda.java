7a8d75f9d8cf2f61cf209a717c08f1b36d718e6d
Steh_Hamashyach <137620415+Hamashyach@users.noreply.github.com>
2024-12-04 20:26:03 -0300

    commit 1

---
+++ QuebraCabeca/src/quebracabeca/Borda.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quebracabeca;
+
+public abstract class Borda {
+    private boolean bd1;
+    private boolean bd2;
+    private boolean bd3;
+    private boolean bd4;
+    
+    public Borda(boolean bd1, boolean bd2, boolean bd3, boolean bd4){
+        this.bd1 = bd1;
+        this.bd2 = bd2;
+        this.bd3 = bd3;
+        this.bd4 = bd4;
+        
+}
+    
+}
---
+++ QuebraCabeca/src/quebracabeca/PecaBase.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quebracabeca;
+
+/**
+ *
+ * @author aluno
+ */
+public abstract class PecaBase {
+    private boolean borda1;
+    
+
+    
+    public PecaBase(){
+      
+    }
+    public void encaixarPeca(){
+        
+    }
+     
+}
