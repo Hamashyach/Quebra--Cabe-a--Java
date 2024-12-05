494e358e5bd4cceaf729a5d3e17f754d9ead987a
Steh_Hamashyach <137620415+Hamashyach@users.noreply.github.com>
2024-12-04 21:47:07 -0300

    Add files via upload

---
+++ quebracabeca/Tabuleiro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quebracabeca;
+
+
+public class Tabuleiro {
+    private final int[][] tabuleiro;
+    
+    public Tabuleiro(){
+        tabuleiro = new int[3][3];
+    }
+    
+    public void AdicionarPeca(){
+        System.out.println("Infelizmente não tenho ideia de como continuar!");
+    }
+}
+    
+    
